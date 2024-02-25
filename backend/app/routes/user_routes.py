706368aacf993efa0cdd582b77c6f199fab12e69
kenji <kenjiginjo@gmail.com>
2024-02-26 01:02:19 +0800

    Add frontend configuration files and update backend routes

--- backend/app/routes/user_routes.py
+++ backend/app/routes/user_routes.py
@@ -3,12 +3,12 @@ from ..models.models import User, db
 
 user_bp = Blueprint('user_bp', __name__)
 
-@user_bp.route('/get_user_list', methods=['GET'])
+@user_bp.route('/users', methods=['GET'])
 def get_user_list():
     users = User.query.all()
     return jsonify([{'uid': user.uid, 'name': user.name, 'gender': user.gender} for user in users])
 
-@user_bp.route('/add_user', methods=['POST'])
+@user_bp.route('/user', methods=['POST'])
 def add_user():
     data = request.json
     user = User(name=data['name'], gender=data['gender'])
@@ -16,7 +16,7 @@ def add_user():
     db.session.commit()
     return jsonify({'message': 'User added successfully'}), 201
 
-@user_bp.route('/edit_user/<int:uid>', methods=['PUT'])
+@user_bp.route('/user/<int:uid>', methods=['PUT'])
 def edit_user(uid):
     user = User.query.filter_by(uid=uid).first()
     if user:
